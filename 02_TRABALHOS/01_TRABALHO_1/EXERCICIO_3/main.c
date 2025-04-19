53fe1196a16f6559d0549230b9c67b69ed721469
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-04-19 15:46:59 -0300

    Resolução completa, exercicio 3 Trabalho 1

--- 02_TRABALHOS/01_TRABALHO_1/EXERCICIO_3/main.c
+++ 02_TRABALHOS/01_TRABALHO_1/EXERCICIO_3/main.c
@@ -43,81 +43,132 @@ int main()
 {
     //CRIAR VARIAVEIS
     int cdgTran, cdgMoto, cdgForn, cdgViag;
+    int qtdForms, qtdNormais, qtdExcedentes;
     float canaKilos, canaTonelada;
     float canaNormal, canaExcedente;
+    float totalCanaNormal, totalCanaExcedente, totalCanaTonelada;
+    float percentualExcedente, percentualNormal;
+    char query;
 
     //INICIALIZAR VARIAVEIS
-    cdgTran = cdgMoto = cdgForn = cdgViag = 0;
-    canaKilos = canaTonelada = 0;
-    canaNormal = canaExcedente = 0;
+    qtdForms = qtdNormais = qtdExcedentes = 0;
+    totalCanaNormal = totalCanaExcedente = 0;
+    percentualExcedente = percentualNormal = 0;
 
-    printf("========================AVALIACAO DE TRANSPORTADORES USINA========================");
-
-    // CODIGO TRANSPORTADOR
-    printf("\nDigite o codigo do transportador [1-99]: ");
     do
     {
-        fflush(stdin);
-        scanf("%d", &cdgTran);
-        if(cdgTran < 1 || cdgTran > 99)
-            printf("\nErro! Digite o codigo no intervalo 1 a 99: ");
-    }while(cdgTran < 1 || cdgTran > 99);
+        // Variaveis que sao 0 a cada laço
+        canaKilos = cdgTran = cdgMoto = cdgForn = cdgViag = 0;
+        query = ' ';
+        canaNormal = canaExcedente = canaTonelada = 0;
+        printf("========================AVALIACAO DE TRANSPORTADORES USINA========================");
+        // CODIGO TRANSPORTADOR
+        printf("\nDigite o codigo do transportador [1-99]: ");
+        do
+        {
+            fflush(stdin);
+            scanf("%d", &cdgTran);
+            if(cdgTran < 1 || cdgTran > 99)
+                printf("\nErro! Digite o codigo no intervalo 1 a 99: ");
+        }while(cdgTran < 1 || cdgTran > 99);
 
-    // CODIGO MOTORISTA
-    printf("\nDigite o codigo do motorista [1-99]: ");
-    do
-    {
-        fflush(stdin);
-        scanf("%d", &cdgMoto);
-        if(cdgMoto < 1 || cdgMoto > 99)
-            printf("\nErro! Digite o codigo no intervalo 1 a 99: ");
-    }while(cdgMoto < 1 || cdgMoto > 99);
+        // CODIGO MOTORISTA
+        printf("\nDigite o codigo do motorista [1-99]: ");
+        do
+        {
+            fflush(stdin);
+            scanf("%d", &cdgMoto);
+            if(cdgMoto < 1 || cdgMoto > 99)
+                printf("\nErro! Digite o codigo no intervalo 1 a 99: ");
+        }while(cdgMoto < 1 || cdgMoto > 99);
 
-    // CODIGO FORNECEDOR
-    printf("\nDigite o codigo do fornecedor [1-99]: ");
-    do
-    {
-        fflush(stdin);
-        scanf("%d", &cdgForn);
-        if(cdgForn < 1 || cdgForn > 99)
-            printf("\nErro! Digite o codigo no intervalo 1 a 99: ");
-    }while(cdgForn < 1 || cdgForn > 99);
+        // CODIGO FORNECEDOR
+        printf("\nDigite o codigo do fornecedor [1-99]: ");
+        do
+        {
+            fflush(stdin);
+            scanf("%d", &cdgForn);
+            if(cdgForn < 1 || cdgForn > 99)
+                printf("\nErro! Digite o codigo no intervalo 1 a 99: ");
+        }while(cdgForn < 1 || cdgForn > 99);
 
-    // CODIGO VIAGEM
-    printf("\nDigite o codigo do viagem [1-99]: ");
-    do
-    {
-        fflush(stdin);
-        scanf("%d", &cdgViag);
-        if(cdgViag < 1 || cdgViag > 99)
-            printf("\nErro! Digite o codigo no intervalo 1 a 99: ");
-    }while(cdgViag < 1 || cdgViag > 99);
+        // CODIGO VIAGEM
+        printf("\nDigite o codigo do viagem [1-99]: ");
+        do
+        {
+            fflush(stdin);
+            scanf("%d", &cdgViag);
+            if(cdgViag < 1 || cdgViag > 99)
+                printf("\nErro! Digite o codigo no intervalo 1 a 99: ");
+        }while(cdgViag < 1 || cdgViag > 99);
 
-    // QUANTIDADE DE CANA EM KG
-    printf("\nInforme a quantidade de cana transportada(kg): ");
-    do
-    {
-        fflush(stdin);
-        scanf("%f", &canaKilos);
-        if(canaKilos <= 0)
-            printf("\nErro! Informe uma quantidade valida!: ");
-    }while(canaKilos <= 0);
+        // QUANTIDADE DE CANA EM KG
+        printf("\nInforme a quantidade de cana transportada(kg): ");
+        do
+        {
+            fflush(stdin);
+            scanf("%f", &canaKilos);
+            if(canaKilos <= 0)
+                printf("\nErro! Informe uma quantidade valida!: ");
+        }while(canaKilos <= 0);
 
-    canaTonelada += canaKilos / 1000.0;
+        canaTonelada += canaKilos / 1000.0;
 
-    if(canaTonelada > 20)
-        canaExcedente = canaTonelada - 20;
-    else
-        canaExcedente = 0;
+        // Quantidade de cana normal e excedente
+        if(canaTonelada > 20)
+        {
+            canaExcedente = canaTonelada - 20;
+            qtdExcedentes++;
+        }
+        else
+        {
+            canaExcedente = 0;
+            qtdNormais++;
+        }
 
-    canaNormal = canaTonelada - canaExcedente;
-    // MOSTRAR RELATORIO
-    printf("\nRELATORIO:");
-    printf("\nForam transporatadas %.2f Toneladas no total.", canaTonelada);
-    printf("\n%.2f Toneladas normais e %.2f Toneladas excedentes.", canaNormal, canaExcedente);
+        canaNormal = canaTonelada - canaExcedente;
+        qtdForms++;
+
+        // Quantidade total de cana normal, cana excedente e cana tonelada
+        totalCanaNormal += canaNormal;
+        totalCanaExcedente += canaExcedente;
+        totalCanaTonelada += canaTonelada;
+
+        system("cls");
+
+        // MOSTRAR RELATORIO
+        printf("RELATORIO DE VIAGEM (%d):", qtdForms);
+        printf("\nCodigo do Transportador: %d", cdgTran);
+        printf("\nCodigo do Motorista....: %d", cdgMoto);
+        printf("\nCodigo do Fornecedor...: %d", cdgForn);
+        printf("\nCodigo da Viagem.......: %d", cdgViag);
+        printf("\nQuantidade de Cana Transportada(KG): %.2f Kilos", canaKilos);
+
+        printf("\n\nPara realizar mais um relatorio aperte a tecla ENTER");
+        printf("\nCaso deseje finalizar, aperte a tecla ESC: ");
+        do
+        {
+            query = getch();
+            if(query != 27 && query != 13)
+                printf("\nTecle apenas ENTER ou ESC: ");
+        }while(query != 27 && query != 13);
+
+        system("cls");
+    }while(query != 27);
+
+    // Percentual de canas excedente
+    percentualExcedente = 100 * (totalCanaExcedente / totalCanaTonelada);
+    percentualNormal = 100 * (totalCanaNormal / totalCanaTonelada);
+    printf("RELATORIO FINAL");
+    printf("\nA) Foram transporatadas %.2f Toneladas no total.", totalCanaTonelada);
+    printf("\nB) %.2f Toneladas normais e %.2f Toneladas excedentes.", totalCanaNormal, totalCanaExcedente);
+    printf("\nC) Ocorreram %d viagens no total.", qtdForms);
+    printf("\nD) Ocorreram %d viagens normais e %d viagens excentes.", qtdNormais, qtdExcedentes);
+    printf("\nE) %.2f%% de cana excedente.", percentualExcedente);
+    printf("\nF) %.2f%% de cana normal.", percentualNormal);
 
     //FINALIZAR PROGRAMA
-    printf ("\n\n\n FIM DO PROGRAMA - VAI EMBORA DAQUI :/ \n\n\n");
+    printf ("\n\n\nFIM DO PROGRAMA - VAI EMBORA DAQUI :/ \n\n\n");
 
     return 0;
 }
