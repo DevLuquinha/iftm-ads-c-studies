f2fd9c66f340453ad60b0b3fe713636c4c3f1882
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-05-19 22:56:35 -0300

    Resolução exercicio 01, Lista 03 Vetores

--- 01_LISTA_EXERCICIOS/06_VETORES/03_LISTA 03/EXERCICIO_01/main.c
+++ 01_LISTA_EXERCICIOS/06_VETORES/03_LISTA 03/EXERCICIO_01/main.c
@@ -65,12 +65,52 @@ int CalculateTotalEven(int vector[], int size)
     return total;
 }
 //===============================================================
+void CreateVectorR1(int vector1[], int sizeVect1, int vector2[], int sizeVect2, int vectorR[])
+{
+    int rIndex = 0;
+
+    for(int i = 0; i < sizeVect1; i++)
+    {
+        if(vector1[i] % 2 == 0)
+        {
+            vectorR[rIndex] = vector1[i];
+
+            for(int j = 0; j < sizeVect2; j++)
+            {
+               vectorR[rIndex] += vector2[j];
+            }
+            rIndex++;
+        }
+    }
+}
+//===============================================================
+void CreateVectorR2(int vector1[], int sizeVect1, int vector2[], int sizeVect2, int vectorR[])
+{
+    int rIndex = 0;
+
+    for(int i = 0; i < sizeVect1; i++)
+    {
+        if(vector1[i] % 2 != 0)
+        {
+            int nDividers = 0;
+            for(int j = 0; j < sizeVect2; j++)
+            {
+                if(vector1[i] % vector2[j] == 0)
+                {
+                    nDividers++;
+                }
+            }
+
+            vectorR[rIndex] = nDividers;
+            rIndex++;
+        }
+    }
+}
+//===============================================================
 void ShowVector(int vector[], int size)
 {
     for(int i = 0; i < size; i++)
-    {
         printf("%d ", vector[i]);
-    }
 }
 //===============================================================
 //==============  CODIGO PRINCIPAL ==============================
@@ -94,14 +134,33 @@ int main()
     int totalOdd = CalculateTotalOdd(vector1, vec1Size);
     int totalEven = CalculateTotalEven(vector1, vec1Size);;
 
+    // Criar e Inicializar Vector R1
+    int vecR1[totalEven];
+    IniciateVector(vecR1, totalEven);
+
+    CreateVectorR1(vector1, vec1Size, vector2, vec2Size, vecR1);
+
+    // Criar e Inicializar Vector R2
+    int vecR2[totalOdd];
+    IniciateVector(vecR2, totalOdd);
+
+    CreateVectorR2(vector1, vec1Size, vector2, vec2Size, vecR2);
+
     // Mostrar vetor 1 e vetor 2
     printf("V1: ");
     ShowVector(vector1, vec1Size);
     printf("\nV2: ");
     ShowVector(vector2, vec2Size);
 
-    // Mostrar quantidade de numeros impares
-    printf("\n\nA quantidade pares e impares: %d e %d", totalEven, totalOdd);
+    printf("\n=================================");
+
+    // Mostrar vetor R1
+    printf("\nR1: ");
+    ShowVector(vecR1, totalEven);
+
+    // Mostrar vetor R1
+    printf("\nR2: ");
+    ShowVector(vecR2, totalOdd);
 
     // FINALIZAR PROGRAMA
     printf ("\n\n\n FIM DO PROGRAMA - VAI EMBORA DAQUI :/ \n\n\n");
