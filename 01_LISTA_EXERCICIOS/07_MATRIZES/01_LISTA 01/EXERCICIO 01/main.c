09b35008800b670508c5bc548e59ba7fe087e3f9
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-06-11 19:54:17 -0300

    Resolução a, b, c do EX 1. Matriz Lista 1

--- 01_LISTA_EXERCICIOS/07_MATRIZES/01_LISTA 01/EXERCICIO 01/main.c
+++ 01_LISTA_EXERCICIOS/07_MATRIZES/01_LISTA 01/EXERCICIO 01/main.c
@@ -32,11 +32,17 @@
 //===============================================================
 void GetName(char name[])
 {
-    // Receber o nome do usuário
-    printf("\nDigite o seu nome: ");
-    fflush(stdin);
-    gets(name);
+    do
+    {
+        printf("Digite o seu nome: ");
+        fflush(stdin);
+        gets(name);
 
+        if(strcmp(name, "") == 0)
+            printf("\nERRO! DIGITE UM NOME !!!\n");
+    }while(strcmp(name, "") == 0);
+
+    system("cls");
 }
 //===============================================================
 void FillMatriz(float matriz[][col])
@@ -67,11 +73,91 @@ void ShowMatriz(float matriz[][col])
     {
         for(int c = 0; c < col; c++)
         {
-            printf("%4.0f ", matriz[r][c]);
+            printf("%9.0f ", matriz[r][c]);
+            Sleep(50);
         }
         printf("\n");
     }
 }
+//===============================================================
+void ClearVectorRow(float vector[], int length)
+{
+    for(int i = 0; i < length; i++)
+        vector[i] = 0.0;
+}
+//===============================================================
+void GetVectorRow(float matriz[][row], float vector[], int length)
+{
+    float sum = 0;
+
+    for(int r = 0; r < row; r++)
+    {
+        sum = 0;
+
+        for(int c = 0; c < col; c++)
+            sum += matriz[r][c];
+
+        vector[r] = sum;
+    }
+}
+//===============================================================
+void ShowVectorRow(float vector[], int length)
+{
+    printf("\nVETOR LINHAS: [ ");
+
+    for(int i = 0; i < length; i++)
+    {
+        if(i + 1 == length)
+            printf("%.0f ]", vector[i]);
+        else
+            printf("%.0f, ", vector[i]);
+
+        Sleep(100);
+    }
+
+//    for(int i = 0; i < length; i++)
+//    {
+//        printf("\n%.0f", vector[i]);
+//    }
+}
+//===============================================================
+//===============================================================
+void ClearVectorColumn(float vector[], int length)
+{
+    for(int i = 0; i < length; i++)
+        vector[i] = 0.0;
+}
+//===============================================================
+void GetVectorColumn(float matriz[][row], float vector[], int length)
+{
+    float sum = 0;
+
+    for(int c = 0; c < col; c++)
+    {
+        sum = 0;
+        for(int r = 0; r < row; r++)
+        {
+            sum += matriz[r][c];
+        }
+        vector[c] = sum;
+    }
+}
+//===============================================================
+void ShowVectorColumn(float vector[], int length)
+{
+    printf("\nVETOR COLUNAS: [ ");
+
+    for(int i = 0; i < length; i++)
+    {
+        if(i + 1 == length)
+            printf("%.0f ]", vector[i]);
+        else
+            printf("%.0f, ", vector[i]);
+
+        Sleep(100);
+    }
+}
+//===============================================================
 //===============================================================
 //==============  CODIGO PRINCIPAL ==============================
 //===============================================================
@@ -80,34 +166,49 @@ int main()
     // Criar variáveis
     float matriz[row][col];
     char name[100];
+    float sumVecRows[row];
+    float sumVecCols[col];
 
     // Limpar a matriz e as variáveis
     ClearMatriz(matriz);
+
     fflush(stdin);
     strcpy(name, "");
 
-    // Exibir Matriz
-    printf("============================\n");
-    printf(" MATRIZ NA FORMA DE ENTRADA");
-    printf("\n============================\n");
+    ClearVectorRow(sumVecRows, row);
+    ClearVectorColumn(sumVecCols, col);
 
-    ShowMatriz(matriz);
-    printf("============================");
-
-    FillMatriz(matriz);
-
-    printf("\n     MATRIZ PREENCHIDA");
-    printf("\n============================\n");
-
-    ShowMatriz(matriz);
-    printf("============================");
-
-    // Receber nome
+    // Atribuir a variavel por referência
     GetName(name);
 
-    // Exibir nome
-    printf("\nSeu nome: %s", name);
+    // Exibição do video
+    printf("=======================================================");
+    printf("\nOla meu querido usuario(a): %s", name);
+    printf("\n=======================================================");
 
+    FillMatriz(matriz);
+    GetVectorRow(matriz, sumVecRows, row);
+    GetVectorColumn(matriz, sumVecCols, col);
+
+    printf("\n              MATRIZ NA FORMA DE ENTRADA");
+    printf("\n=======================================================\n");
+
+    ShowMatriz(matriz);
+    printf("=======================================================");
+
+    printf("\n               VETOR DA SOMA DAS LINHAS");
+    printf("\n=======================================================");
+
+    ShowVectorRow(sumVecRows, row);
+
+    printf("\n=======================================================");
+
+    printf("\n               VETOR DA SOMA DAS COLUNAS");
+    printf("\n=======================================================");
+
+    ShowVectorColumn(sumVecCols, col);
+
+    printf("\n=======================================================");
     // FINALIZAR PROGRAMA
     printf ("\n\n\n FIM DO PROGRAMA - VAI EMBORA DAQUI :/ \n\n\n");
 
