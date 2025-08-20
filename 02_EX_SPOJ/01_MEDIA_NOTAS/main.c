6da328c3b2517c3f275e9798a2b773d8cf3c5631
Lucas IFTM <lucas.emmanuel@estudante.iftm.edu.br>
2025-08-20 19:56:08 -0300

    Create the GetDataStudent function

--- 02_EX_SPOJ/01_MEDIA_NOTAS/main.c
+++ 02_EX_SPOJ/01_MEDIA_NOTAS/main.c
@@ -18,15 +18,39 @@ int GetAmountStudents()
     return q;
 }
 
+Student GetDataStudent()
+{
+    Student student;
+
+    fflush(stdin);
+    gets(student.name);
+
+    fflush(stdin);
+    scanf("%i", &student.registry);
+
+    for(int i = 0; i < 3; i++)
+    {
+        fflush(stdin);
+        scanf("%f", &student.grades[i]);
+    }    
+
+    return student;
+}
+
 int main()
 {
-    int amountS;
-    // Vetor de estudantes
-
     // Quantidade N de estudantes
+    int amountS;
+    
+    // Vetor de estudantes
+    Student students[1000];
+    
     amountS = GetAmountStudents();
 
+    Student student = GetDataStudent(students[0]);
+
     printf("\nA quantidade estudantes eh %i", amountS);
+    printf("\n1. %s (%i): %f", student.name, student.registry);
     // Pegar as (nome, matricula, n1, n2 e n3) dos N estudantes
 
     // Imprimir no formato posicao. nome (matricula): media
