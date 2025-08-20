e620e9333b784c2189bf5763606a9320361d8de0
Lucas IFTM <lucas.emmanuel@estudante.iftm.edu.br>
2025-08-20 19:37:27 -0300

    Create the student struct and the GetAmountStudents()

--- 02_EX_SPOJ/01_MEDIA_NOTAS/main.c
+++ 02_EX_SPOJ/01_MEDIA_NOTAS/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+struct student
+{
+    char name[50];
+    int registry;
+    float grades[3];    
+};
+typedef struct student Student;
+
+int GetAmountStudents()
+{
+    int q;
+    fflush(stdin);
+    scanf("%i", &q);
+
+    return q;
+}
+
 int main()
 {
-    // Quantidade N de estudantes
+    int amountS;
+    // Vetor de estudantes
 
+    // Quantidade N de estudantes
+    amountS = GetAmountStudents();
+
+    printf("\nA quantidade estudantes eh %i", amountS);
     // Pegar as (nome, matricula, n1, n2 e n3) dos N estudantes
 
     // Imprimir no formato posicao. nome (matricula): media
