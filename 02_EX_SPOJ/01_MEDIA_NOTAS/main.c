462f96ece8fb811efd879c22f201868b6470fc72
Lucas IFTM <lucas.emmanuel@estudante.iftm.edu.br>
2025-08-20 19:27:13 -0300

    Create the EX_SPOJ dir and the first ex structure

---
+++ 02_EX_SPOJ/01_MEDIA_NOTAS/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    // Quantidade N de estudantes
+
+    // Pegar as (nome, matricula, n1, n2 e n3) dos N estudantes
+
+    // Imprimir no formato posicao. nome (matricula): media
+
+    return 0;
+}
