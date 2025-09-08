6abfede8c96b6b400d58e472954dd46b19329c43
Lucas <lucas.emmanuel@estudante.iftm.edu.br>
2025-09-08 20:47:58 -0300

    Create the struct node, understanding lists

---
+++ 01_AULAS/EX-Lista/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct node
+{
+    int info;
+    struct node* next;
+};
+typedef struct node Node;
+
+int main()
+{
+    printf("Hello World!");
+
+    return 0;
+}
