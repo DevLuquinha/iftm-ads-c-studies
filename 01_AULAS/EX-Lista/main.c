a36094f8405971f068662ce2c5c3e1d0d55e6aec
Lucas <lucas.emmanuel@estudante.iftm.edu.br>
2025-09-08 20:53:46 -0300

    Create the NewNode Function

--- 01_AULAS/EX-Lista/main.c
+++ 01_AULAS/EX-Lista/main.c
@@ -9,8 +9,26 @@ struct node
 };
 typedef struct node Node;
 
+Node* NewNode(int value)
+{
+    // Inicializar a estrutura
+    Node* new = NULL;
+    new = malloc(sizeof(Node));
+
+    // Atribuir os valores
+    new->info = value;
+    new->next = NULL;
+
+    return new;
+}
+
 int main()
 {
+    // Inicializar a estrutura
+    Node* node = NULL;
+
+    
+
     printf("Hello World!");
 
     return 0;
