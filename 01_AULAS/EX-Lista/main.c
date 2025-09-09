519ea2ddd3699290d72284074915c9131debfa93
Lucas <lucas.emmanuel@estudante.iftm.edu.br>
2025-09-08 22:09:53 -0300

    Create the AddEndValue function

--- 01_AULAS/EX-Lista/main.c
+++ 01_AULAS/EX-Lista/main.c
@@ -22,14 +22,57 @@ Node* NewNode(int value)
     return new;
 }
 
+Node* AddStartValue(Node* list, int value)
+{
+    Node* node;
+    if(list == NULL)
+    {
+        list = NewNode(value);
+    }
+    else
+    {
+        node = NewNode(value);
+        node->next = list;
+        list = node;
+    }
+    
+    return list;
+}
+
+Node* AddEndValue(Node* list, int value)
+{
+    // Começa com a lista
+    Node* currentNode = list;
+
+    // 1. Verificar se a lista é NULL
+    if(list == NULL)
+    {
+        list = NewNode(value);
+    }
+    else
+    {
+        // 2. Laço até chegar no NULL
+        while(currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+        }
+        
+        // 3. Atribuir o valor para o ultimo nó
+        currentNode->next = NewNode(value);
+    }
+    
+    return list;
+}
+
 int main()
 {
-    // Inicializar a estrutura
-    Node* node = NULL;
-    node = NewNode(10);
+    // Inicializar a lista
+    Node* list = NULL;
+    
+    list = AddStartValue(list, 10);
+    list = AddStartValue(list, 20);
 
-    printf("TESTE\n");
-    printf("%i", node->info);
+    list = AddEndValue(list, 3);
 
     return 0;
 }
