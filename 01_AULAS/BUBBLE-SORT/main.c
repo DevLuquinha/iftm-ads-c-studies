38c9d8f8b57ecde80ebb97a120e48b31e74913f6
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-09-11 17:42:01 -0300

    Create the ShowVector()

--- 01_AULAS/BUBBLE-SORT/main.c
+++ 01_AULAS/BUBBLE-SORT/main.c
@@ -9,10 +9,18 @@ void SortByAlph(char vecNames[][20], int vecLen)
     }
 }
 
+void ShowVector(char vecNames[][20], int vecLen)
+{
+    for(int i = 0; i < vecLen; i++)
+    {
+        printf("%s\n", vecNames[i]);
+    }
+}
+
 int main()
 {
     char names[5][20] = {"Alice", "Bob", "Charlie", "Diana", "Eve"};
-    SortByAlph(names, 5);
+    ShowVector(names, 5);
 
     return 0;
 }
