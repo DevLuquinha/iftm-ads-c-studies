073816a2531fc8f689e94a547165d03fff680e3a
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-09-11 18:06:53 -0300

    Calculate the BubbleSort with nums

--- 01_AULAS/BUBBLE-SORT/main.c
+++ 01_AULAS/BUBBLE-SORT/main.c
@@ -3,9 +3,26 @@
 
 void SortByAlph(char vecNames[][20], int vecLen)
 {
-    for(int i = 0; i < vecLen; i++)
+    int vec[5] = {3, 1, 8, 2, 5};
+    int aux;
+
+    // Repetir o vecLen vezes
+    for(int i = 0; i < 5; i++)
     {
-        printf("%s\n", vecNames[i]);
+        for (int j = i; j < vecLen - 1; j++)
+        {
+            if (vec[i] < vec[j+1])
+            {
+                aux = vec[j+1];
+                vec[j+1] = vec[i];
+                vec[i] = aux;
+            }
+        }
+    }
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%i, ", vec[i]);
     }
 }
 
@@ -20,7 +37,14 @@ void ShowVector(char vecNames[][20], int vecLen)
 int main()
 {
     char names[5][20] = {"Alice", "Bob", "Charlie", "Diana", "Eve"};
-    ShowVector(names, 5);
+    
+    // printf("================ NOMES ================\n");
+    // ShowVector(names, 5);
+
+    SortByAlph(names, 5);
+
+    // printf("================ NOMES ORDENADOS ================\n");
+    // ShowVector(names, 5);
 
     return 0;
 }
