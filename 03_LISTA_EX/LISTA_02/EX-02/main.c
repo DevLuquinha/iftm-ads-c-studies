7968e6f8b995ee40b69188e9ae75ec5091dfea5f
Lucas IFTM <lucas.emmanuel@estudante.iftm.edu.br>
2025-09-22 21:18:59 -0300

    Finish the Ex 2, List 2

--- 03_LISTA_EX/LISTA_02/EX-02/main.c
+++ 03_LISTA_EX/LISTA_02/EX-02/main.c
@@ -9,10 +9,79 @@ struct list
 };
 typedef struct list List; 
 
-// List* CreateList(int arrayLen, int* array)
-// {
+List* NewNode(int value)
+{
+    List* newNode = malloc(sizeof(List));
+    newNode->value = value;
+    newNode->next = NULL;
 
-// }
+    return newNode;
+}
+
+void AddValue(List* list, int value)
+{
+    List* auxList = list;
+
+    while(auxList->next != NULL)
+    {
+        auxList = auxList->next;
+    }
+
+    // End node
+    auxList->next = NewNode(value);
+}
+
+List* CreateList(int arrayLen, int* array)
+{
+    List* list = NULL;
+
+    // 1. Array is empty
+    if(arrayLen > 0)
+    {
+        // Foreach elem in array
+        for(int i = 0; i < arrayLen; i++)
+        {
+            if(i == 0)
+            {
+                list = NewNode(array[i]);
+            }    
+            else
+            {
+                AddValue(list, array[i]);
+            }
+        }
+    }
+
+    return list;
+}
+
+void ShowArray(int len, int array[])
+{
+    printf("{");
+    for(int i = 0; i < len; i++)
+    {
+        printf("%i", array[i]);
+        if(i+1 != len)
+            printf(", ");
+    }
+    printf("}");
+}
+
+void ShowList(List* list)
+{
+    printf("[");
+
+    while(list != NULL)
+    {
+        printf("%i", list->value);
+        list = list->next;
+
+        if(list != NULL)
+            printf(", ");
+    }
+    printf("]");
+
+}
 
 int main()
 {
@@ -21,7 +90,13 @@ int main()
     // OBS: If array is empty the function will return null list.
 
     int array[LEN] = {3, 8, 1, 7, 2};
-    
-    printf("Hello World!");
+    List* list = CreateList(LEN, array);
+
+    printf("ARRAY: ");
+    ShowArray(LEN, array);
+
+    printf("\nLIST : ");
+    ShowList(list);
+
     return 0;
 }
