f344b9e3ed9f1a795462deaf44e368de3def0a10
Lucas IFTM <lucas.emmanuel@estudante.iftm.edu.br>
2025-09-22 21:49:27 -0300

    Finish the Ex 03 - List 2

--- 03_LISTA_EX/LISTA_02/EX-03/main.c
+++ 03_LISTA_EX/LISTA_02/EX-03/main.c
@@ -10,11 +10,61 @@ struct listStudent
 };
 typedef struct listStudent ListStudent;
 
+ListStudent* NewNode(char* name, float grade)
+{
+    ListStudent* node = malloc(sizeof(ListStudent));
+    strcpy(node->name, name);
+    node->grade = grade;
+    node->next = NULL;
+    
+    return node;
+}
+
+ListStudent* AddStudent(ListStudent* list, char* name, float grade)
+{
+    ListStudent* aux;
+    ListStudent* newNode = NewNode(name, grade);
+
+    // 1. The list is NULL?
+    if(list != NULL)
+    {
+        aux = list;
+        list = newNode;
+        list->next = aux;
+    }
+    else
+    {
+        list = newNode;
+    }
+
+    return list;
+}
+
+void ShowList(ListStudent* list)
+{
+    printf("[");
+    while(list != NULL)
+    {
+        printf("{NOME: %s, NOTA: %.2f}", list->name, list->grade);
+        list = list->next;
+        if(list != NULL)
+            printf(", ");
+    }
+    printf("]");
+}
+
 int main()
 {
     // Create a function that add a student in linked List
     // The function must return a list pointer
 
-    printf("Hello World!");
+    ListStudent* list = NULL;
+    list = AddStudent(list, "Lucas Emmanuel", 15.5);
+    list = AddStudent(list, "Felipe Lara", 25.5);
+    list = AddStudent(list, "Eduardo", 0.5);
+    
+    printf("LISTA: ");
+    ShowList(list);
+
     return 0;
 }
