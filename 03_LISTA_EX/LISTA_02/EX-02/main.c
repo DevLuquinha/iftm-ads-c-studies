65e42bd87b6963a23c8cf9e3abdc2ab003331e79
Lucas IFTM <lucas.emmanuel@estudante.iftm.edu.br>
2025-09-22 21:00:32 -0300

    Init the Ex 2, list 2

--- 03_LISTA_EX/LISTA_02/EX-02/main.c
+++ 03_LISTA_EX/LISTA_02/EX-02/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define LEN 5
+
+struct list
+{
+    int value;
+    struct list* next;
+};
+typedef struct list List; 
+
+// List* CreateList(int arrayLen, int* array)
+// {
+
+// }
 
 int main()
 {
+    // Implement a function that recieve a int array with n elements
+    // and create a linked list from array elements.
+    // OBS: If array is empty the function will return null list.
+
+    int array[LEN] = {3, 8, 1, 7, 2};
+    
     printf("Hello World!");
-    return 999;
+    return 0;
 }
