4764f55b2e7dd8fc0224c1648305024c9470163a
Lucas IFTM <lucas.emmanuel@iftm.edu.br>
2025-10-01 19:34:03 -0300

    Organize the exercise list and create the EX-01][

---
+++ 03_LISTA_EX/LISTA_02/02_LISTAS_DUPLAS/EX-01/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node
+{
+    int value;
+    struct node* prev;
+    struct node* next;
+};
+typedef struct node Node;
+
+struct list
+{
+    Node* init;
+    Node* end;
+};
+typedef struct list List;
+
+int main()
+{
+    // Escreva uma função que retire o último elemento de uma dada lista.
+    // A função deve retornar o ponteiro para a lista alterada
+
+    List* defaultList = NULL;
+
+    printf("Hello World");
+    return 0;
+}
