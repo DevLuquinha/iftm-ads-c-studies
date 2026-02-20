93a3aa81bee9d6fcee1fc215341a069c5e074446
Dev Luquinha <lucas.emmanuel@estudante.iftm.edu.br>
2026-02-20 20:21:26 -0300

    Create the NewNode() function

--- 01-binary-tree/main.c
+++ 01-binary-tree/main.c
@@ -9,6 +9,16 @@ struct node
 };
 typedef struct node Node;
 
+Node* NewNode(int value)
+{
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->value = value;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
 int main()
 {
     printf("Hello world!\n");
