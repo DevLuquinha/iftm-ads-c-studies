5a5f3652a3abf13d67e2b4b6e2d39af530712c07
Dev Luquinha <lucas.emmanuel@estudante.iftm.edu.br>
2026-02-20 20:28:28 -0300

    Create the InsertNode function

--- 01-binary-tree/main.c
+++ 01-binary-tree/main.c
@@ -19,6 +19,35 @@ Node* NewNode(int value)
     return node;
 }
 
+Node* InsertNode(int value, Node* root)
+{
+    Node* newNode = NewNode(value);
+
+    // 1. The Root is null
+    if (root == NULL)
+    {
+        return newNode;
+    }
+    else
+    {
+        // 2. The value on right
+        if (value > root->value)
+        {
+            root->right = InsertNode(value, root->right);
+        }
+        else if (value < root->value)
+        {
+            root->left = InsertNode(value, root->left);
+        }
+        else
+        {
+            printf("\nThe %i already exists in the tree!", value);
+        }
+
+        return root;
+    }
+}
+
 int main()
 {
     printf("Hello world!\n");
