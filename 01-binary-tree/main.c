43cffb476a15362d944e20f3e7fbbb5a0c088bb2
Dev Luquinha <lucas.emmanuel@estudante.iftm.edu.br>
2026-02-24 20:02:48 -0300

    Create the FindNode for search a node in BST

--- 01-binary-tree/main.c
+++ 01-binary-tree/main.c
@@ -19,6 +19,15 @@ Node* NewNode(int value)
     return node;
 }
 
+int GetInt()
+{
+    int num = 0;
+    fflush(stdin);
+    scanf("%i", &num);
+
+    return num;
+}
+
 Node* InsertNode(int value, Node* root)
 {
     Node* newNode = NewNode(value);
@@ -58,19 +67,57 @@ void ShowPreOrder(Node* root)
     }
 }
 
+Node* FindNode(Node* root, int value)
+{
+    if (root != NULL)
+    {
+        // 1. The root is the value target  [1, 2, 3, 4, 5]
+        if (root->value == value)
+        {
+            return root;
+        }
+        else if (value > root->value) // Value on right
+        {
+            root = FindNode(root->right, value);
+        }
+        else if (value < root->value) // Value on left
+        {
+            root = FindNode(root->left, value);
+        }
+    }
+
+    return root;
+}
+
 int main()
 {
     printf("The Binary Search Tree");
     Node* root = NULL;
-    int values[11] = {50, 30, 70, 20, 40, 60, 80, 35, 45, 65, 85};
+//    int values[11] = {50, 30, 70, 20, 40, 60, 80, 35, 45, 65, 85};
+    int values[9] = { 8, 4, 12, 2, 6, 10, 14, 1, 3 };
 
-    for(int i = 0; i < 11; i++)
+    for(int i = 0; i < 9; i++)
     {
         root = InsertNode(values[i], root);
     }
 
     printf("\n\nShow Pre Order Binary Tree: ");
+
     ShowPreOrder(root);
+
+    printf("\n\nType the target value: ");
+    int num = GetInt();
+
+    Node* targetNode = FindNode(root, num);
+
+    if (targetNode != NULL)
+    {
+        printf("\nThe value %i is on the tree", targetNode->value);
+    }
+    else
+    {
+        printf("\nThe value %i doesn't exists in tree", num);
+    }
 
     return 0;
 }
