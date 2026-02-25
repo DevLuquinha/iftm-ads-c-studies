c54769412c467a834862b0a9f3f7844630467701
Dev Luquinha <lucas.emmanuel@estudante.iftm.edu.br>
2026-02-24 21:30:37 -0300

    Create the tree height function

--- 02-binary-tree-assignment-01/Assignment-01/main.c
+++ 02-binary-tree-assignment-01/Assignment-01/main.c
@@ -1,8 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LEN 15
+
+struct node
+{
+    int value;
+    int heightLevel;
+    struct node* left;
+    struct node* right;
+};
+typedef struct node Node;
+
+Node* NewNode(int value, int heightLevel)
+{
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->left = NULL;
+    node->right = NULL;
+    node->value = value;
+    node->heightLevel = heightLevel;
+
+    return node;
+}
+
+Node* InsertNode(Node* root, int value, int heightLevel)
+{
+    // 1. The root is null
+    if (root == NULL)
+    {
+        Node* node = NewNode(value, heightLevel);
+        return node;
+    }
+    else
+    {
+        // 2. The node on right
+        if (value > root->value)
+        {
+            root->right = InsertNode(root->right, value, heightLevel + 1);
+        }
+        else if (value < root->value) // 3. The node on left
+        {
+            root->left = InsertNode(root->left, value, heightLevel + 1);
+        }
+
+        return root;
+    }
+}
+
+Node* GetTreeHeight(Node* root, int* maxHeight)
+{
+    if (root != NULL)
+    {
+        if (root->heightLevel > *maxHeight)
+        {
+            *maxHeight = root->heightLevel;
+        }
+        GetTreeHeight(root->left, maxHeight);
+        GetTreeHeight(root->right, maxHeight);
+    }
+}
+
+void ShowPreOrder(Node* root)
+{
+    if (root != NULL)
+    {
+        printf("%i, ", root->value);
+        ShowPreOrder(root->left);
+        ShowPreOrder(root->right);
+    }
+}
+
+void ShowInOrder(Node* root)
+{
+    if (root != NULL)
+    {
+        ShowInOrder(root->left);
+        printf("%i, ", root->value);
+        ShowInOrder(root->right);
+    }
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    int values[LEN] = { 25, 12, 40, 9, 18, 33, 50, 29, 37, 45, 60, 15, 20, 48, 55 };
+
+    Node* root = NULL;
+
+    for(int i = 0; i < LEN; i++)
+    {
+        root = InsertNode(root, values[i], 1);
+    }
+
+    printf("The tree IN ORDER: ");
+    ShowInOrder(root);
+
+    int treeHeight = 0;
+    GetTreeHeight(root, &treeHeight);
+
+    printf("\nThe Tree Height is %i\n", treeHeight);
+
     return 0;
 }
