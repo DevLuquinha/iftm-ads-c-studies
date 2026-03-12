e1a1e3a6bac19d9e1110f8c40e142803daad20ea
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-03-12 18:57:37 -0300

    Create the insertNode function for avl bst

--- 05-avl-tree-assignment-02/main.c
+++ 05-avl-tree-assignment-02/main.c
@@ -35,6 +35,33 @@ int getHeight(Node* root){
     }
 }
 
+int getBalanceFactor(Node* root){
+    if (root == NULL){
+        return 0;
+    }
+
+    int leftHeight = getHeight(root->left);
+    int heightHeight = getHeight(root->right);
+
+    return leftHeight - heightHeight;
+}
+
+Node* rotateLeft(Node* unbalanced){
+    Node* child = unbalanced->right;
+    unbalanced->right = child->left;
+    child->left = unbalanced;
+    
+    return child;
+}
+
+Node* rotateRight(Node* unbalanced){
+    Node* child = unbalanced->left;
+    unbalanced->left = child->right;
+    child->right = unbalanced;
+
+    return child;
+}
+
 Node* insertNode(Node* root, int value){
     // 1. Check if the node is a leaf
     if (root == NULL){
@@ -49,6 +76,31 @@ Node* insertNode(Node* root, int value){
     } else if (value > root->value){
         // 2.2 Node is on right
         root->right = insertNode(root->right, value);
+    }
+
+    // 3. Get node balance factor
+    int balanceFactor = getBalanceFactor(root);
+
+    // 4.1 Simple Rotation left
+    if (balanceFactor < -1 && value > root->right->value){
+        return rotateLeft(root);
+    }
+
+    // 4.2 Simple Rotation right
+    if (balanceFactor > 1 && value < root->left->value){
+        return rotateRight(root);
+    }
+
+    // 4.3 Double Rotation left_right
+    if (balanceFactor > 1 && value > root->left->value){
+        root->left = rotateLeft(root->left);
+        return rotateRight(root);
+    }
+
+    // 4.4 Double Rotation right_left
+    if (balanceFactor < -1 && value < root->right->value){
+        root->right = rotateRight(root->right);
+        return rotateLeft(root);
     }
 
     return root;
@@ -84,7 +136,7 @@ int main(){
         root = insertNode(root, values[i]);
     }
 
-    Node* targetNode = getNode(root, 8);
+    Node* targetNode = getNode(root, 45);
     if (targetNode != NULL){
         int height = getHeight(targetNode);
         printf("The height is %d", height);
