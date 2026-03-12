a8ce724d50a64bffc2577f53eebc3cc1116abd17
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-03-12 18:31:37 -0300

    Create the getHeight function

--- 05-avl-tree-assignment-02/main.c
+++ 05-avl-tree-assignment-02/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LEN 22
+#define LEN 20
 
 struct node
 {
@@ -18,6 +18,21 @@ Node* newNode(int value){
     node->value = value;
 
     return node;
+}
+
+int getHeight(Node* root){
+    if (root == NULL){
+        return 0;
+    }
+
+    int lefthHeight = getHeight(root->left);
+    int rightHeight = getHeight(root->right);
+
+    if (lefthHeight > rightHeight){
+        return lefthHeight + 1;
+    } else {
+        return rightHeight + 1;
+    }
 }
 
 Node* insertNode(Node* root, int value){
@@ -47,16 +62,36 @@ void showInOrder(Node* root){
     }
 }
 
+Node* getNode(Node* root, int value){
+    if (root == NULL){
+        return NULL;
+    }
+
+    if (value < root->value){
+        return getNode(root->left, value);
+    } else if (value > root->value){
+        return getNode(root->right, value);
+    } 
+
+    return root;
+}
+
 int main(){
-    int values[LEN] = { 0, 10, 20, 40, 50, 25, 45, 43, 41, 70, 65, 15, 5, 8, 55, 58, 57, 75, 80, 78 };
+    int values[LEN] = { 30, 10, 20, 40, 50, 25, 45, 43, 41, 70, 65, 15, 5, 8, 55, 58, 57, 75, 80, 78 };
     Node* root = NULL;
 
     for(int i = 0; i < LEN; i++){
         root = insertNode(root, values[i]);
     }
 
-    printf("The BST: ");
-    showInOrder(root);
+    Node* targetNode = getNode(root, 8);
+    if (targetNode != NULL){
+        int height = getHeight(targetNode);
+        printf("The height is %d", height);
+    }
+
+    // printf("The BST: ");
+    // showInOrder(root);
 
     return 0;
 }
