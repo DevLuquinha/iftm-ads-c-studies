a990c73782bbc596df6e6c62cc0bbae090ec3ddb
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-03-13 18:19:00 -0300

    Practing rotate nodes in avl bst

--- 04-avl-tree-study/main.c
+++ 04-avl-tree-study/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LEN 16 // 3
+#define LEN 3
 
 struct node
 {
-    int value;
     struct node* left;
     struct node* right;
-    int height;
+    int value;
 };
 typedef struct node Node;
 
@@ -17,27 +16,22 @@ Node* newNode(int value){
     node->left = NULL;
     node->right = NULL;
     node->value = value;
-    node->height = 0;
 
     return node;
 }
 
-int getHeight(Node* node){
-    if (node == NULL){
-        return -1;
+int getHeight(Node* root){
+    if (root == NULL){
+        return 0;
     }
 
-    return node->height;
-}
+    int leftHeight = getHeight(root->left);
+    int rightHeight = getHeight(root->right);
 
-void updateHeight(Node* node){
-    int heightLeft = getHeight(node->left);
-    int heightRight = getHeight(node->right);
-
-    if (heightLeft > heightRight){
-        node->height = heightLeft + 1;
+    if (leftHeight > rightHeight){
+        return leftHeight + 1;
     } else {
-        node->height = heightRight + 1;
+        return rightHeight + 1;
     }
 }
 
@@ -46,47 +40,85 @@ int getBalanceFactor(Node* root){
         return 0;
     }
 
-    int factor = getHeight(root->left) - getHeight(root->right);
-    return factor;
+    int leftH = getHeight(root->left);
+    int rightH = getHeight(root->right);
+
+    return leftH - rightH;
+}
+
+Node* rotateLeft(Node* unbalance){
+    Node* child = unbalance->right;
+    unbalance->right = child->left;
+    child->left = unbalance;
+
+    return child;
+}
+
+Node* rotateRight(Node* unbalance){
+    Node* child = unbalance->left;
+    unbalance->left = child->right;
+    child->right = unbalance;
+
+    return child;
 }
 
 Node* insertNode(Node* root, int value){
-    // 1. If the node is a leaf
     if (root == NULL){
         Node* node = newNode(value);
         return node;
     }
 
-    // 2. Normal insert flow
-    if (value > root->value){
+    if (value < root->value){
+        root->left = insertNode(root->left, value); 
+    } else if (value > root->value){
         root->right = insertNode(root->right, value);
-    } else if (value < root->value){
-        root->left = insertNode(root->left, value);
     }
 
-    // 3. Calculate the height value
-    updateHeight(root);
-
-    // 4. Calculate de balance factor
     int balanceFactor = getBalanceFactor(root);
-    
+
+    // 1. Simple Rotation left
+    if (balanceFactor < -1 && value > root->right->value){
+        return rotateLeft(root);
+    }
+
+    // 2. Simple Rotation right
+    if (balanceFactor > 1 && value < root->left->value){
+        return rotateRight(root);
+    }
+
+    // 3. Double Rotation left_right
+    if (balanceFactor > 1 && value > root->left->value){
+        root->left = rotateLeft(root->left);
+        return rotateRight(root);
+    }
+
+    // 4. Double Rotation right_left
+    if (balanceFactor < -1 && value < root->right->value){
+        root->right = rotateRight(root->right);
+        return rotateLeft(root);
+    }
+
     return root;
 }
 
+void showInOrder(Node* node){
+    if (node != NULL){
+        showInOrder(node->left);
+        printf("%i, ", node->value);
+        showInOrder(node->right);
+    }
+}
+
 int main(){
-    // a) 10 - 15 - 20
-    // b) 20 - 15 - 10
-    // c) 15 - 20 - 10
-    // d) 10 - 20 - 15
+    int values[LEN] = { 20, 15, 10 };
     Node* root = NULL;
-    // int values[LEN] = { 10, 25, 20 };
-    int values[LEN] = { 25, 12, 40, 9, 18, 33, 50, 29, 37, 45, 60, 15, 15, 20, 48, 55 }; 
-    
-    for (int i = 0; i < LEN; i++){
+
+    for (int i = 0; i < LEN; i++)
+    {
         root = insertNode(root, values[i]);
     }
     
-    printf("Hello world!");
+    showInOrder(root);
 
     return 0;
 }
